761537cda7991f09ea135e9136e0a2b3946ee5c3
andrea.bortolossi <andrea.bortolossi89@gmail.com>
2017-01-10 15:40:41 +0100

    aggiunto commento nel file ImageRecognitionConfig.java

--- src/com/it/ibm/watson/config/ImageRecognitionConfig.java
+++ src/com/it/ibm/watson/config/ImageRecognitionConfig.java
@@ -6,6 +6,8 @@ public class ImageRecognitionConfig {
 	public static final String url = "https://gateway-a.watsonplatform.net/visual-recognition/api";
 //	public static final String api_key = "64d19574fb8f93071a94a461e479ff6c6885d78b";
 
+	
+	//COMMENTO DI PROVA
 
 	// Luca credenziali
 	public static final String api_key = "a40fce6329c185129d0d6ac72f4a4b22d23ffba1";
